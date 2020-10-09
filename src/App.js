bdcb0ce81b1a394a66d71741d6a24d81410014cd
waseem-wassu <waseemwassu8873@gmail.com>
2020-10-09 17:59:39 +0530

    creating login form and navigation bar with the help of material ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import ButtonAppBar from "./NavBar";
+
+const useStyles = makeStyles((theme) => ({
+
+  paper: {
+    width:400,
+    margin: '0 auto',
+    marginTop:'130px',
+    textAlign: 'center',
+    padding: 15
+  },
+  Input : {
+    width:"95%",
+    marginBottom: 10
+  }
+}));
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const classes = useStyles();
+
+    return (
+      <div className={classes.root}>
+         <ButtonAppBar />
+          <Paper elevation={3} className={classes.paper} >
+              <h1>Login</h1>
+              <form className={classes.root} noValidate autoComplete="off">
+                <TextField  label="Enter Email" className={classes.Input} />
+                <TextField  label="Enter Password" className={classes.Input} />
+                  <Button variant="contained" color="primary">
+                      Login
+                  </Button>
+              </form>
+          </Paper>
+        </div>
+    );
 }
 
 export default App;
